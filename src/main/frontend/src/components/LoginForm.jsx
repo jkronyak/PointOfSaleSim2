9e12c7ef9d52e984eef2ff65b0826dbfea9bf9d4
jkronyak <56709613+jkronyak@users.noreply.github.com>
2022-07-26 16:28:25 -0400

    some login page stuff

--- src/main/frontend/src/components/LoginForm.jsx
+++ src/main/frontend/src/components/LoginForm.jsx
@@ -2,15 +2,18 @@ import React from "react";
 
 function LoginForm(props) {
     return (
-        <div>
+        <div class="login-form">
             <form>
                 <label htmlFor="empId">Employee ID: </label>
-                <input id="empId" type="text"/>
+                <input id="empId" type="number" min="1"/>
 
                 <label htmlFor="password">Password: </label>
                 <input id="password" type="password"/>
+                <button type="submit">Login</button>
             </form>
+
         </div>
+
     )
 }
 
